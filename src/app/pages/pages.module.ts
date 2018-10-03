23de2379fb2bc9824bda86c0680271f6edd53830
jsanchezd <jsanchezd@itteria.com>
2018-10-03 10:07:48 +0200

    agregamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+//Modulos
+import { NgModule } from "@angular/core";
+// Componentes
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+import { PagesComponent } from "./pages.component";
+import { SharedModule } from "../shared/shared.module";
+// Rutas
+import { PAGES_ROUTES } from "./pages.routes";
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  imports: [
+    SharedModule,
+    PAGES_ROUTES
+  ]
+})
+
+export class PagesModule { }
